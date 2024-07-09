9720706b3c920821135ea3bed4f7ed475e36681d
keyamoni05866 <keya05866@gmail.com>
2024-07-09 22:44:39 +0600

    server setup done

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response, application } from "express";
+import cors from "cors";
+import router from "./app/routes";
+import notFoundRoute from "./app/middlewares/notFoundRoute";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+
+const app: Application = express();
+
+//parser
+app.use(express.json());
+app.use(cors());
+
+//application routes
+
+app.use("/api", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Campers Shop Shop Server");
+});
+
+app.use(globalErrorHandler);
+app.use(notFoundRoute);
+export default app;
---
+++ src/app/modules/product/product.route.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const router = express.Router();
+
+// router.post(
+//   "/signup",
+//   validateRequest(userValidations),
+//   AuthControllers.signup
+// );
+// router.post(
+//   "/signin",
+//   validateRequest(signInValidation),
+//   AuthControllers.signIn
+// );
+
+export const ProductRoute = router;
